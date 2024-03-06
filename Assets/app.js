9ae0c581fde454146871889ed995233e7d9c2566
pointlessbish <87618683+pointlessbish@users.noreply.github.com>
2024-03-06 03:37:03 +0100

    updated

--- Assets/app.js
+++ Assets/app.js
@@ -7,4 +7,12 @@ document.getElementById("cards").onmousemove = e => {
       card.style.setProperty("--mouse-x", `${x}px`);
       card.style.setProperty("--mouse-y", `${y}px`);
     };
+    for(const listcontent of document.getElementsByClassName("listcontent")) {
+      const rect = listcontent.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      listcontent.style.setProperty("--mouse-x", `${x}px`);
+      listcontent.style.setProperty("--mouse-y", `${y}px`);
+    };
   }
