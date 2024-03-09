ba398f0221737e17fbeabc9e9d2210d613d39277
pointlessbish <87618683+pointlessbish@users.noreply.github.com>
2024-03-09 15:17:38 +0100

    removed clutter

--- Assets/app.js
+++ Assets/app.js
@@ -1,31 +1,35 @@
-document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
-    for(const listcontent of document.getElementsByClassName("listcontent")) {
-      const rect = listcontent.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      listcontent.style.setProperty("--mouse-x", `${x}px`);
-      listcontent.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
+document.getElementById( "cards" ).onmousemove = e =>
+{
+  for ( const card of document.getElementsByClassName( "card" ) )
+  {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty( "--mouse-x", `${ x }px` );
+    card.style.setProperty( "--mouse-y", `${ y }px` );
+  };
+  for ( const listcontent of document.getElementsByClassName( "listcontent" ) )
+  {
+    const rect = listcontent.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    listcontent.style.setProperty( "--mouse-x", `${ x }px` );
+    listcontent.style.setProperty( "--mouse-y", `${ y }px` );
+  };
+};
 
 
-  const sidebar = document.querySelector(".sidebar");
-  const toggleBtn = document.querySelector(".menu-btn");
-  const spacer = document.querySelector(".spacer");
-  // const cards = document.querySelector("#cards");
+const sidebar = document.querySelector( ".sidebar" );
+const toggleBtn = document.querySelector( ".menu-btn" );
+const spacer = document.querySelector( ".spacer" );
+// const cards = document.querySelector("#cards");
 
-  toggleBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-    spacer.classList.toggle("active");
-   // toggleBtn.classList.toggle("close-icon");
-   // cards.classList.toggle("active");
-  })
+toggleBtn.addEventListener( "click", () =>
+{
+  sidebar.classList.toggle( "active" );
+  spacer.classList.toggle( "active" );
+  // toggleBtn.classList.toggle("close-icon");
+  // cards.classList.toggle("active");
+} );
